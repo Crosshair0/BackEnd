43c1cec59f819125aed1691851efe9ea9e6b7289
Abel Fabian <Abelperez218@gmail.com>
2023-02-06 17:56:15 -0300

    editing cross origin

--- src/main/java/com/portfolio/afp/Controller/CHys.java
+++ src/main/java/com/portfolio/afp/Controller/CHys.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/skill")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://frontendprueba-3c683.web.app"})
 public class CHys {
 
     @Autowired
